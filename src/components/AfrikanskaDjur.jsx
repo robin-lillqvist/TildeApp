91c03c9cfbed956452ab6cede9429f54bab5ab2b
robin-lillqvist <robin.lillqvist@gmail.com>
2020-04-08 09:27:50 +0200

    updates the pages

--- src/components/AfrikanskaDjur.jsx
+++ src/components/AfrikanskaDjur.jsx
@@ -7,15 +7,18 @@ import hippoAudio from '../sounds/hippo2.mp3'
 import tigerAudio from '../sounds/Tiger4.mp3'
 import zebraAudio from '../sounds/zebra7.mp3'
 
-function importAll(r) {
-  let images = {};
-  r.keys().map((item, index) => { images[item.replace('./', '')] = r(item); });
-  return images;
+function importAll (r) {
+  let images = {}
+  r.keys().map((item, index) => {
+    images[item.replace('./', '')] = r(item)
+  })
+  return images
 }
-const images = importAll(require.context('../images/afrikanska', false, /\.(png|jpe?g|svg)$/));
+const images = importAll(
+  require.context('../images/afrikanska', false, /\.(png|jpe?g|svg)$/)
+)
 
 const ExploreContainer = () => {
-
   let elephant = new Audio(elephantAudio)
   let rhino = new Audio(rhinoAudio)
   let camel = new Audio(camelAudio)
@@ -26,12 +29,36 @@ const ExploreContainer = () => {
   return (
     <>
       <div className='container'>
-        <img onClick={() => elephant.play()} src={images['elephant.png']} alt="dog"/>
-        <img onClick={() => rhino.play()} src={images['rhino.png']} alt="rhino"/>
-        <img onClick={() => camel.play()} src={images['camel.png']} alt="camel"/>
-        <img onClick={() => hippo.play()} src={images['hippo.png']} alt="hippo"/>
-        <img onClick={() => tiger.play()} src={images['tiger.png']} alt="tiger"/>
-        <img onClick={() => zebra.play()} src={images['zebra.png']} alt="zebra"/>
+        <img
+          onClick={() => elephant.play()}
+          src={images['elephant.png']}
+          alt='dog'
+        />
+        <img
+          onClick={() => rhino.play()}
+          src={images['rhino.png']}
+          alt='rhino'
+        />
+        <img
+          onClick={() => camel.play()}
+          src={images['camel.png']}
+          alt='camel'
+        />
+        <img
+          onClick={() => hippo.play()}
+          src={images['hippo.png']}
+          alt='hippo'
+        />
+        <img
+          onClick={() => tiger.play()}
+          src={images['tiger.png']}
+          alt='tiger'
+        />
+        <img
+          onClick={() => zebra.play()}
+          src={images['zebra.png']}
+          alt='zebra'
+        />
       </div>
     </>
   )
